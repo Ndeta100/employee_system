b027d9ce836b068ac9b1b6a749b7c7033a40f9a0
ndeta <ndetainnocent3@gmail.com>
2023-04-30 01:52:46 +0300

    implementing the controller and fixing service Impl

---
+++ src/main/java/com/ndeta/employeesystem/services/EmployeeServiceImpl.java
@@ -0,0 +1,48 @@
+package com.ndeta.employeesystem.services;
+
+import com.ndeta.employeesystem.entity.Employee;
+import com.ndeta.employeesystem.repository.EmployeeRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService{
+    EmployeeRepository employeeRepository;
+    @Override
+    public Employee createEmployee(Employee employee) {
+        Employee employee1=new Employee();
+        BeanUtils.copyProperties(employee1,employee);
+        return employee;
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Boolean deleteEmployee(Long id) {
+        Employee employee=employeeRepository.findById(id).get();
+        employeeRepository.delete(employee);
+        return true;
+    }
+
+    @Override
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).get();
+    }
+
+    @Override
+    public Employee updateEmployee(Long id, Employee employee) {
+        Employee employee1=employeeRepository.findById(id).get();
+        employee1.setEmailId(employee.getEmailId());
+        employee1.setFirstName(employee.getFirstName());
+        employee1.setLastName(employee.getLastName());
+        employeeRepository.save(employee1);
+        return employee;
+    }
+}
---
+++ src/main/java/com/ndeta/employeesystem/web/EmployeeController.java
@@ -0,0 +1,47 @@
+package com.ndeta.employeesystem.web;
+
+import com.ndeta.employeesystem.entity.Employee;
+import com.ndeta.employeesystem.services.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+    EmployeeService employeeService;
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return  employeeService.createEmployee(employee);
+    }
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return  employeeService.getAllEmployees();
+    }
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
+        boolean deleted=false;
+        deleted=employeeService.deleteEmployee(id);
+        Map<String, Boolean>response=new HashMap<>();
+        response.put("deleted",deleted);
+        return ResponseEntity.ok(response);
+    }
+    @GetMapping("/employee/{id}")
+    public ResponseEntity<Employee>getEmployeeById(@PathVariable Long id){
+        Employee employee=null;
+        employee=employeeService.getEmployeeById(id);
+        return ResponseEntity.ok(employee);
+    }
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,
+                                                   @RequestBody Employee  employee){
+        employee=employeeService.updateEmployee(id,employee);
+        return ResponseEntity.ok(employee);
+    }
+}
